0717ea1d8188789d9dab2acb0a179f8492a1c0a4
artyom <abazh0307@gmail.com>
2026-02-08 19:56:02 +0300

    task 1.4-1.5

--- Lab1/rect.cpp
+++ Lab1/rect.cpp
@@ -1,1 +1,20 @@
 #include "rect.hpp"
+#include <iostream>
+//Конструктор по умолчанию
+Rect::Rect() {
+    top = 0;
+    left = 0;
+    bottom = 0;
+    right = 0;
+}
+//Конструктор с параметрами
+Rect::Rect(int t, int l, int b, int r) {
+    top = t;
+    left = l;
+    bottom = b;
+    right = r;
+}
+//Деструктор
+Rect::~Rect() {
+    std::cout << "Destructor called for Rect: " << top << " " << left << std::endl;
+}
--- Lab1/rect.hpp
+++ Lab1/rect.hpp
@@ -1,18 +1,12 @@
 #pragma once
+#include <iostream>
+
 class Rect {
 public:
     int top, left, bottom, right;
-    Rect(int t,int l,int b,int r){
-        top = t;
-        left = l;
-        bottom = b;
-        right = r;
-    }
-    // Конструктор по умолчанию (Задание 1.3, часть 3)
-    Rect() {
-        top = 0;
-        left = 0;
-        bottom = 0;
-        right = 0;
-    }
+    //Объявления
+    Rect(); // Конструктор по умолчанию
+    Rect(int t, int l, int b, int r); // Конструктор с параметрами
+    ~Rect(); // Деструктор
 };
+
